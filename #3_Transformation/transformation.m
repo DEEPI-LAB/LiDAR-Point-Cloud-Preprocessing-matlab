dab51b3d6cf2827ba8ca01f46f737dfcd888d969
ASPL COMPUTER <jw.kim@sch.ac.kr>
2021-01-25 11:37:04 +0900

    #3 Transformation

--- #3_Transformation/transformation.m
+++ #3_Transformation/transformation.m
@@ -3,6 +3,7 @@ function output = transformation(input, angle)
 x = input;
 z  =deg2rad(angle);
 
+% Transformation
 u_1 = [cos(z) ; 0; -sin(z)];
 u_2 = [0 ; 1 ;0];
 u_3 = [sin(z) ; 0 ;  cos(z)];
@@ -12,9 +13,6 @@ x_p = x(:,1);
 y_p = x(:,2);
 z_p = x(:,3);
 
-new_x = (inv(P)*[x_p' ; y_p' ; z_p'])';
-new_x(:,1) = new_x(:,1);
-new_x(:,2) = new_x(:,2);
-
+new_x = (P*[x_p' ; y_p' ; z_p'])';
 output= new_x;
 end
